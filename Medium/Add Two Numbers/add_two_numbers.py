682a9db617b8c6d8c67e76e4e6d9447c9012786e
ZiqinZhouGit <39079266+ZiqinZhouGit@users.noreply.github.com>
2018-10-04 15:36:03 +1000

    Create add_two_numbers.py

---
+++ Medium/Add Two Numbers/add_two_numbers.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        root = cur_pointer = ListNode(0) 
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, value = divmod(v1+v2+carry, 10)
+            cur_pointer.next = ListNode(value)
+            cur_pointer = cur_pointer.next
+        return root.next
+# root = [0,7,0,8]
+# root.next = [7,0,8]
+# cur_pointer = [8]
